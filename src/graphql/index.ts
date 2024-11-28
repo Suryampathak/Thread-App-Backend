341ecfae4e8651b72befe96ef22ed8376b883d9b
Suryampathak <suryampathak9999@gmail.com>
2024-11-28 13:56:15 +0530

    user data fetched

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,6 +5,7 @@ async function createApolloGraphqlServer() {
     const gqlServer = new ApolloServer({
         // Schema
         typeDefs: `
+            ${User.typeDefs}
             type Query {
                 ${User.queries}
             }
